121d90396df376f0120689f6fb302bc89a5bff84
Eero Vilpponen <eero.vilpponen@gmail.com>
2025-06-14 23:42:43 +0300

    Auto clef

--- scripts/clean_score/clean_score.py
+++ scripts/clean_score/clean_score.py
@@ -17,6 +17,98 @@ LYRICS_BY_TIMEPOS: Dict[str, List[Dict[str, Any]]] = {}
 RESOLUTION: int = 128  # Default resolution for durations in MuseScore XML
 
 
+def detect_part_types(root: etree._Element) -> None:
+    """
+    For each staff, return a clef type and part name.
+    E.g. TTBB
+    T1, T2, B1, B2
+    G8vb, G8vb, F, F
+    """
+    any_f_clef: bool = False
+    # First pass: Find F clefs
+    for staff in root.findall(".//Score/Staff"):
+        clef: Optional[etree._Element] = staff.find(".//Clef")
+        if clef is not None and clef.find(".//concertClefType") is not None:
+            clef_type: str = clef.find(".//concertClefType").text.strip()
+            if clef_type == "F":
+                any_f_clef = True
+                break
+
+    logging.debug(f"Any F clef found: {any_f_clef}")
+    # F clefs are male voices, either T, "Men", or "Baritone" or "Bass"
+
+    part_info = {}
+
+    for staff in root.findall(".//Score/Staff"):
+        clef: Optional[etree._Element] = staff.find(".//Clef")
+        clef_type = None
+        if clef is not None and clef.find(".//concertClefType") is not None:
+            clef_type: str = clef.find(".//concertClefType").text.strip()
+
+        if clef_type is None:
+            clef_type = "G"  # Default to G clef if not found
+
+        # Find highest and lowest notes in the staff
+        highest_note: Optional[int] = None
+        lowest_note: Optional[int] = None
+        for note in staff.findall(".//Note"):
+            pitch_el: Optional[etree._Element] = note.find(".//pitch")
+            if pitch_el is not None and pitch_el.text is not None:
+                pitch: int = int(pitch_el.text)
+                if highest_note is None or pitch > highest_note:
+                    highest_note = pitch
+                if lowest_note is None or pitch < lowest_note:
+                    lowest_note = pitch
+
+        part_name = ""
+        if clef_type == "F":
+            # lowest note < 43 == This is Bass
+            # highest note > 65 == This is Tenor
+            if lowest_note is not None and lowest_note < 43:
+                part_name = "Bass"
+            elif highest_note is not None and highest_note > 65:
+                part_name = "Tenor"
+        elif clef_type == "G":
+            # lowest note < 55 == This is tenor
+            if lowest_note is not None and lowest_note < 55:
+                part_name = "Tenor"
+                clef_type = "G8vb"  # Tenor clef is G8vb
+            if highest_note is not None and highest_note > 72:
+                part_name = "Soprano"
+                clef_type = "G"
+            elif highest_note is not None and highest_note > 68:
+                # Only allow alto if soprano already exists
+                if any(
+                    [
+                        part_info[staff_id]["part_name"] == "Soprano"
+                        for staff_id in sorted(part_info.keys())
+                    ]
+                ):
+                    part_name = "Alto"
+                    clef_type = "G"
+
+        part_info[int(staff.get("id"))] = {
+            "clef_type": clef_type,
+            "highest_note": highest_note,
+            "lowest_note": lowest_note,
+            "part_name": part_name,
+            "part_slug": part_name[0] if part_name else "",
+        }
+
+    sorted_staff_ids: List[int] = sorted(part_info.keys())
+    index = 1
+    prev_part_name: Optional[str] = None
+    for staff_id in sorted_staff_ids:
+        if prev_part_name and part_info[staff_id]["part_name"] != prev_part_name:
+            index = 1
+        part_info[staff_id]["part_index"] = index
+        index += 1
+        prev_part_name = part_info[staff_id]["part_name"]
+
+    logging.debug(f"Part info: {json.dumps(part_info, indent=2)}")
+    return part_info
+
+
 def add_missing_ties(root):
     # Find all tied notes (two notes each)
     # Check all other staffs, if they have same lenght notes at the same time position
@@ -24,7 +116,7 @@ def add_missing_ties(root):
     tied_notes_by_measure_time_pos: Dict[Tuple[int, int], List[etree._Element]] = (
         defaultdict(list)
     )
-    for staff in root.findall(".//Staff"):
+    for staff in root.findall(".//Score/Staff"):
         span_index = None
         for el in loop_staff(staff):
             if el["element"].tag == "Chord":
@@ -49,7 +141,7 @@ def add_missing_ties(root):
     logging.debug(
         f"Found {tied_notes_by_measure_time_pos.keys()} tied notes by measure and time position"
     )
-    for staff in root.findall(".//Staff"):
+    for staff in root.findall(".//Score/Staff"):
         span_index = None
         new_tied_notes = []
         for el in loop_staff(staff):
@@ -1322,6 +1414,41 @@ def main(input_path: str, output_path: str) -> None:
 
     add_missing_ties(root)
 
+    part_types = detect_part_types(root)
+    # Apply part name
+    for part in root.findall(".//Part"):
+        staff: Optional[etree._Element] = part.find(".//Staff")
+        if staff is not None:
+            staff_id: int = int(staff.get("id"))
+            if staff_id in part_types:
+                part_name = part_types[staff_id].get("part_name", "")
+                part_slug = part_types[staff_id].get("part_slug", "")
+                part_index = part_types[staff_id].get("part_index", 1)
+                track_name = part.find(".//trackName")
+                if track_name is not None:
+                    track_name.text = f"{part_slug}{part_index}"
+                long_name = part.find(".//longName")
+                if long_name is not None:
+                    long_name.text = f"{part_name} {part_index}"
+
+    # apply clef
+    for staff in root.findall(".//Score/Staff"):
+        staff_id: int = int(staff.get("id", "0"))
+        if staff_id in part_types:
+            clef_type: Optional[str] = part_types[staff_id].get("clef_type", None)
+            if clef_type is not None:
+                clef = staff.find(".//Clef")
+                if clef is not None:
+                    concert_clef_type = clef.find(".//concertClefType")
+                    if concert_clef_type is not None:
+                        concert_clef_type.text = clef_type
+                        logging.debug(
+                            f"Set concertClefType to {clef_type} for staff {staff_id}"
+                        )
+                    transposing_clef_type = clef.find(".//transposingClefType")
+                    if transposing_clef_type is not None:
+                        transposing_clef_type.text = clef_type
+
     # Serialize the output XML
     output_content: str = etree.tostring(
         root, pretty_print=True, encoding="UTF-8"
@@ -1349,3 +1476,4 @@ if __name__ == "__main__":
         logging.info(f"Output written to {args.output}")
     except Exception as e:
         logging.error(f"An error occurred during conversion: {e}")
+        raise
