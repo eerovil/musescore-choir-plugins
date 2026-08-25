e685181df030254a3af38a2812862170fd5a04f1
Eero Vilpponen <eero.vilpponen@gmail.com>
2026-08-25 08:32:50 +0300

    Pin PWA shell cache generation

---
+++ src/song_app/static/pwa-assets.js
@@ -0,0 +1,1 @@
+self.SONG_PWA = Object.freeze({"cache":"song-static-2ed587400bdc","assets":["/style.css","/pwa.css","/app.js","/rendering_state.js","/pwa.js","/offline.html","/favicon.svg","/apple-touch-icon.png","/icon-192.png","/icon-512.png","/icon-maskable-512.png","/manifest.webmanifest"]});
