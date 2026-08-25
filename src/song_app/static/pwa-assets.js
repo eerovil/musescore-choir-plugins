d04283aa7698029dbd0b198fe5b2a92897d56cef
Eero Vilpponen <eero.vilpponen@gmail.com>
2026-08-25 14:25:02 +0300

    Regenerate PWA asset generation

--- src/song_app/static/pwa-assets.js
+++ src/song_app/static/pwa-assets.js
@@ -1,1 +1,1 @@
-self.SONG_PWA = Object.freeze({"cache":"song-static-b72f38d8f1f2","assets":["/style.css","/pwa.css","/app.js","/rendering_state.js","/pwa.js","/offline.html","/favicon.svg","/apple-touch-icon.png","/icon-192.png","/icon-512.png","/icon-maskable-512.png","/manifest.webmanifest"]});
+self.SONG_PWA = Object.freeze({"cache":"song-static-dedc3754f2f5","assets":["/style.css","/pwa.css","/app.js","/rendering_state.js","/pwa.js","/offline.html","/favicon.svg","/apple-touch-icon.png","/icon-192.png","/icon-512.png","/icon-maskable-512.png","/manifest.webmanifest"]});
